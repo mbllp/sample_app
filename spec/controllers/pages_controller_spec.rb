52f72854c78cd26b9a0facde182b969e7440e93c
Wade Cohn <wcohn@icontrol.com>
2014-01-08 09:27:57 -0600

    changed home content

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,10 +2,17 @@ require 'spec_helper'
 
 describe PagesController do
 
+  render_views
+  
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'home'
+      expect(response.body).to match /MAN'S CHIEF END IS TO GLORIFY GOD AND ENJOY HIM FOREVER./m
     end
   end
 
